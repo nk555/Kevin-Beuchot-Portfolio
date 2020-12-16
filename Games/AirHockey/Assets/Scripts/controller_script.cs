3aa6550d2173a2e1156e7683437a1a7ca3415f59
nivek555 <nkwyywkn@gmail.com>
2020-12-15 22:21:43 -0600

    Webscrapping (almost) done

--- Games/AirHockey/Assets/Scripts/controller_script.cs
+++ Games/AirHockey/Assets/Scripts/controller_script.cs
@@ -2,17 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controller_script : MonoBehaviour
+public abstract class controller_script : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
+    }
+
+    void Move(Vec2 direction){
+
+    }
+
+    void Direction(){
+
+    }
+
+    void Speed(){
+
+    }
+
+    public void onCollision Collider2D(){
+
+    }
+
+}
+
+public class human_controller : controller_script{
+
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    void Move(Vec2 direction){
+
+    }
+
+    void Direction(){
+
+    }
+
+    void Speed(){
+
+    }
+
+    void Gyroscope(){
+        
+    }
+
+    public void onCollision Collider2D(){
+
+    }
+}
+
+public class computer_controller : controller_script{
+
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    void Move(Vec2 direction){
+
+    }
+
+    void Direction(){
+
+    }
+
+    void Speed(){
+
+    }
+
+    public void onCollision Collider2D(){
+
     }
 }
